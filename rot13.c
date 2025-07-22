1c1dde2b1e9a88b17feb4b39fdf0cbcc3250518c
Segen Stoutamire <72527185+clifflocked@users.noreply.github.com>
2025-07-21 17:27:54 -0700

    Make rot13.c pass -Wall

--- rot13.c
+++ rot13.c
@@ -2,41 +2,42 @@
 #include <stdlib.h>
 
 int main(int argc, char * argv[]) {
-  if (argc == 1) {
-    printf("Usage: %s WORD...\n", argv[0]);
-    return 2;
-  }
-
-  for (int j = 1; j < argc; j++) { 
-  int strlen = 0;
-  for (int i = 0; argv[1][i] != '\0'; i++)
-    strlen++;
-
-    unsigned char * arg = (char *)calloc(1, strlen + 1);
-
-    for (int i = 0; i <= strlen; i++)
-      arg[i] = argv[1][i];
-
-    for (int i = 0; arg[i] != '\0'; i++) {
-      if (97 <= arg[i] && arg[i] <= 122) {
-        arg[i] += 13;
-        
-        if (arg[i] > 122)
-          arg[i] -= 26;
-      } else if (65 <= arg[i] && arg[i] <= 90) {
-        arg[i] += 13;
-
-        if (arg[i] > 90)
-          arg[i] -= 26;
-      }
-
-      putchar(arg[i]);
+    if (argc == 1) {
+      printf("Usage: %s WORD...\n", argv[0]);
+      return 2;
     }
 
-    putchar(' ');
+    for (int j = 1; j < argc; j++) { 
+        int strlen = 0;
 
-    free(arg);
-  }
+        for (int i = 0; argv[1][i] != '\0'; i++)
+            strlen++;
+
+        unsigned char * arg = (unsigned char *)calloc(1, strlen + 1);
+
+        for (int i = 0; i <= strlen; i++)
+            arg[i] = argv[1][i];
+
+        for (int i = 0; arg[i] != '\0'; i++) {
+            if (97 <= arg[i] && arg[i] <= 122) {
+                arg[i] += 13;
+        
+                if (arg[i] > 122)
+                  arg[i] -= 26;
+            } else if (65 <= arg[i] && arg[i] <= 90) {
+                arg[i] += 13;
+
+                if (arg[i] > 90)
+                  arg[i] -= 26;
+            }
+
+            putchar(arg[i]);
+        }
+
+        putchar(' ');
+
+        free(arg);
+    }
 
   putchar('\n');
 }
